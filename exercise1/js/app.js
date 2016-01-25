fcac1ea92813be3ea44fe2a71761cb4bf9f04196
Zhang Shuai <520zhangshuai@gmail.com>
2016-01-25 14:04:18 +0800

    Implement exercise1.js.

--- exercise1/js/app.js
+++ exercise1/js/app.js
@@ -1,4 +1,49 @@
+// Header will show the title of this page;
+// SearchBar include a input edit;
+// EmployeeList will show a hardcode employee's list
+
+var Header = React.createClass({
+    render: function () {
+        return (
+            <h1>This is header with title: {this.props.title}</h1>
+        );
+    }
+});
+
+var SearchBar = React.createClass({
+    render: function () {
+        return (
+            <input id="searchInput"/>
+        );
+    }
+});
+
+var EmployeeList = React.createClass({
+    render: function () {
+        return (
+            <ul>
+                <li>James King</li>
+                <li>Julie Taylor</li>
+                <li>Eugene Lee</li>
+            </ul>
+        );
+    }
+});
+
+var HomePage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Header title="Employee List"/>
+                <SearchBar />
+                <EmployeeList />
+            </div>
+        );
+
+    }
+});
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <HomePage />,
     document.body
 );
